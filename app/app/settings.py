ab823f31651edbd65ad847366d641ba13c0abd03
Hugo Nijdam <HuGoN@Laptop.nl>
2024-09-29 19:54:00 +0200

    Postgres db added

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7c)_@qnp#gdd@x9$$1b*))bb2ql0i0syr1kc3+%i5ivd579lr!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
