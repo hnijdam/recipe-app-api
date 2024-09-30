abed13e50778e9a6197c3a3c58e73b4d06c4baf4
Hugo Nijdam <HuGoN@Laptop.nl>
2024-09-30 21:24:48 +0200

    added more unit tests, and defs of user auth

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,8 +8,8 @@ from django.contrib.auth import get_user_model
 class ModelTests(TestCase):
     """Test models."""
 
-    def test_create_user_with_email_succesfull(self):
-        """Test creating a user with an email is succesful."""
+    def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is successful."""
         email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
@@ -17,5 +17,17 @@ class ModelTests(TestCase):
             password=password,
         )
 
-        self.assert_Equal(user.email, email)
+        self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users."""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.com', 'test4@example.com'],
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
